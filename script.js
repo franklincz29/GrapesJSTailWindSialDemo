f80cb6facff5acefaba06b8e8ea9884256b6588e
franklincz29 <franklin.cz29@gmail.com>
2025-07-10 15:50:15 -0600

    cambiar el acceso a la imagen desde el header

--- script.js
+++ script.js
@@ -28,11 +28,17 @@ fetch(projectBase + 'header.html')
     // Agregar botón de logout si está logueado
     if (localStorage.getItem('logueado') === 'true') {
       const userActions = document.getElementById('user-actions');
-      if (userActions) {
+      const dashboardLink = document.getElementById('dashboard-link');
+      if (userActions&&dashboardLink) {
         const logoutBtn = document.createElement('button');
         logoutBtn.textContent = 'Cerrar sesión';
         logoutBtn.className = 'ml-4 text-white bg-red-500 px-4 py-2 rounded hover:bg-red-600';
         logoutBtn.addEventListener('click', logout);
+        const dashboarLink = document.createElement('a')
+        dashboarLink.textContent = 'Dashboard';
+        dashboarLink.className='class="mr-5 hover:text-black-900"';
+        dashboarLink.href='dashboard.html';
+        userActions.appendChild(dashboardLink);
         userActions.appendChild(logoutBtn);
       }
     }
